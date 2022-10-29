d4519687fb10f252b4d3b15d16f9ef86e054f5b9
userjohn <johnchacpi479@gmail.com>
2022-10-29 17:48:28 -0500

    created print value and clear functions

--- tictactoe/src/js/index.js
+++ tictactoe/src/js/index.js
@@ -14,6 +14,27 @@ let player1, player2 = {
 buttons = Array.from(buttons)
 board.classList.add("disabled");
 
-function printValue(){
-    
+function clearProcess() {
+    currentSlot.classList.add("disabled");
+    currentSlot.removeEventListener("click", printValue);
+    listOfMovements = listOfMovements.filter(elem => elem.id !== currentSlot.id)
+    checkWinner()
+}
+
+function printValue(event){
+    const target = event.target
+    currentSlot = target;
+
+    if(player1.active) {
+        target.innerText = player1.value;
+        player1.active = false;
+        player2.active = true;
+        clearProcess();
+    }
+    else {
+        target.innerText = player2.value;
+        player1.active = true;
+        player2.active = false;
+        clearProcess();
+    }
 }
