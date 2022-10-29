0d5d8fd8d67bda6139533c4f0b16f7dd6e2114b8
userjohn <johnchacpi479@gmail.com>
2022-10-28 23:29:11 -0500

    started js logic

---
+++ tictactoe/src/js/index.js
@@ -0,0 +1,15 @@
+const board = document.querySelector("#board");
+const modal = document.querySelector("#modal");
+const slotsList = [];
+const rows = [];
+let movementsList = [];
+let currentSlot;
+
+let buttons = document.querySelectorAll(".option-button");
+
+let player1, player2 = {
+    active: false,
+    value: ""
+}
+buttons = Array.from(buttons)
+board.classList.add("disabled");
