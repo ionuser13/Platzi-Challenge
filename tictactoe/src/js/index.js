48f6b695e251a6ee76fee003ef8d6d2ce097c085
userjohn <johnchacpi479@gmail.com>
2022-11-01 14:03:37 -0500

    logic to color three cells and determinate winner

--- tictactoe/src/js/index.js
+++ tictactoe/src/js/index.js
@@ -19,7 +19,7 @@ function startGame(){
     originBoard = Array.from(Array(9).keys());//makes the array to be every number from 0 to 9
     for (let i = 0; i < cells.length; i++){
         cells[i].innerText = "";
-        cells[i].style.removeProperty("background.color");
+        cells[i].style.removeProperty("background-color");
         cells[i].addEventListener("click", turnClick, false);
     }
 }
@@ -30,4 +30,26 @@ function turnClick(square) {
 function turn(squareId, player) {
     originBoard[squareId] = player;
     document.getElementById(squareId).innerText = player;
+    let gameWon = checkWin(originBoard, player)
+    if(gameWon) gameOver(gameWon);
 } 
+function checkWin(board, player) {
+    let plays = board.reduce((acumulator, element, index) => (element ===player)?acumulator.concat(index): acumulator, []);// finds every index that the player has played in
+    let gameWon = null;
+    for(let [index, win] of winCombos.entries()){
+        if(win.every(elem => plays.indexOf(elem) > -1)) {
+            gameWon = {index: index, player: player};
+            break;
+        }
+    }
+    return gameWon
+}
+
+function gameOver(gameWon) {
+    for(let index of winCombos[gameWon.index]) {
+        document.getElementById(index).style.backgroundColor = gameWon.player == player ? "blue": "red";
+    }
+    for (let i = 0; i < cells.length; i++) {
+        cells[i].removeEventListener("click", turnClick, false)
+    }
+}
