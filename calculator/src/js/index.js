5f1f1739ccd68c53686cc57ba514ed3525f8c305
userjohn <johnchacpi479@gmail.com>
2022-11-02 18:56:48 -0500

    implemented calculation functionality

--- calculator/src/js/index.js
+++ calculator/src/js/index.js
@@ -26,7 +26,29 @@ class Calculator {
         this.currentOperand = "";
     }
     compute() {
-        
+        let computation;
+        const prev = parseFloat(this.previousOperand);
+        const current = parseFloat(this.currentOperand);
+        if(isNaN(prev) || isNaN(current)) return
+        switch(this.operation) {
+            case "+":
+                computation = prev + current;
+                break;
+            case "-":
+                computation = prev - current;
+                break;
+            case "*":
+                computation = prev * current;
+                break;
+            case "÷":
+                computation = prev / current;
+                break;
+            default:
+                return
+        }
+        this.currentOperand = computation;
+        this.operation = undefined;
+        this.previousOperand = "";
     }
     updateDisplay(){
         this.currentOperandText.innerText = this.currentOperand;
@@ -55,4 +77,8 @@ operationButtons.forEach(button => {
         calculator.chooseOperation(button.innerText)
         calculator.updateDisplay();
     })
+})
+equalsButton.addEventListener("click", button => {
+    calculator.compute();
+    calculator.updateDisplay();
 })
